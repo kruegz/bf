7bfb4499106c8677723b764c368320a7bfc3879e
kruegz <krueger.zach@gmail.com>
2015-10-29 14:26:15 -0500

    Add comments

--- bf.c
+++ bf.c
@@ -3,8 +3,10 @@
 
 int main(int argc, char const *argv[])
 {
+	// Setup write and read files
 	FILE *fr, *fw;
 
+	// Check arguments
 	if (argc < 2)
 	{
 		printf("%s\n", "Usage:\t./bf filename");
@@ -12,12 +14,14 @@ int main(int argc, char const *argv[])
 		exit(1);
 	}
 
+	// Get the filename
 	char const * file = argv[1];
 
 	// Try to load file data
 	fr = fopen(file, "r");
 	fw = fopen("out.c", "w");
 
+	// Check file
 	if (fr == NULL)
 	{
 		printf("%s\n", "Invalid file name.");
@@ -29,14 +33,17 @@ int main(int argc, char const *argv[])
 		exit(1);
 	}
 
+	// Write headers
 	fprintf(fw,"#include \"stdio.h\"\n\n");
 	fprintf(fw,"int main()\n{\n");
 	fprintf(fw,"char array[30000] = {0};\nchar *ptr=array;\n");
-
+	
+	// Get characters from the input file
 	char c;
 	while (!feof(fr))
 	{
 		c = getc(fr);
+		// Write the appropriate statement to the file
 		switch (c)
 		{
 			case '>':
@@ -66,11 +73,14 @@ int main(int argc, char const *argv[])
 		}
 	}
 
+	// Write the closing statments
 	fprintf(fw, "return 0;\n}\n");
 
+	// Close the files
 	fclose(fw);
 	fclose(fr);
 
+	// Print confirmation
 	printf("%s\n", "File successfully written to out.c");
 
 	return 0;
